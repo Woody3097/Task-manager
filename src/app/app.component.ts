746887ea715534524db129746775aa32ef032820
NicolayDanch <nicolay.danch@qroot.app>
2024-12-29 16:04:37 +0200

    master: add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [RouterLink, RouterOutlet],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
